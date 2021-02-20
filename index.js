b301e1f0081c4dab50994cbfe4062f025875c7f9
Mengqi Wang <wmq516@gmail.com>
2021-02-20 10:59:26 -0700

    added get route for employee profiles

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const express = require("express")
-const cors = require("cors")
+const express = require('express')
+const cors = require('cors')
+const fetch = require('node-fetch')
 const app = express()
 
+// middlewares
 app.use(cors())
 // app.use(express.json())
 
+// ROUTES
+app.get('/api/employees', (req, res) => {
+    const baseUrl = 'https://randomuser.me/api/?results=2'
+
+    fetch(baseUrl)
+        .then(res => res.json())
+        .then(data => res.send(data))
+        .catch(err => console.error(err))
+
+})
+
 app.listen(5000, () => {
-    console.log("server has started on port 5000")
+    console.log('server has started on port 5000')
 })
